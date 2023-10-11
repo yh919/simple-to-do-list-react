b63580cecfee76cf6273c2a45240e928aa336011
Youssef Hussein <youssefalcon30@gmail.com>
2023-10-11 22:41:10 +0300

    Simple To-Do-List Front-End

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([])
+  
+  const inputRef = useRef();
+
+  const HandleAddTodo = () => {
+    const text = inputRef.current.value;
+    const newItem = {completed : false , text}
+    if (text.length <= 0) {
+      return;
+    } else {
+      setTodos([...todos, newItem]);
+      inputRef.current.value = "";
+    }
+  }
+  
+  const handleItemDone = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  }
+
+  const handleDeleteItem = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>To Do List</h2>
+      <div className='to-do-container'>
+      <ul>
+        {todos.map(({text , completed} , index) => {
+          return (
+            <div className='todos'>
+            <li className={completed ? "done" : ""} key={index} onClick={() => {
+              handleItemDone(index)
+              }}>{text}
+              </li>
+              <span className='deleteItem' onClick={() => handleDeleteItem(index)}>✖️</span>
+            </div>
+              )
+        })}
+        </ul>
+        <div className='AddItems'>
+      <input ref={inputRef} placeholder='Enter item ...' />
+      <button onClick={HandleAddTodo}>Add</button>
+        </div>
+      </div>
     </div>
   );
 }
